7bfe9a018c17c583063024e0dab70cd925cc8872
Tommaso Fonda <fondatommaso2@gmail.com>
2022-04-18 13:50:24 +0200

    msm8974: camera: include display_headers
    Change-Id: If599470763c021c35f07b76b7fb9ad5415e1b8e8

--- camera/QCamera2/HAL/Android.mk
+++ camera/QCamera2/HAL/Android.mk
@@ -50,7 +50,7 @@ LOCAL_SHARED_LIBRARIES := libcamera_client liblog libhardware libutils libcutils
 LOCAL_SHARED_LIBRARIES += libmmcamera_interface libmmjpeg_interface
 LOCAL_SHARED_LIBRARIES += android.hidl.token@1.0-utils android.hardware.graphics.bufferqueue@1.0
 LOCAL_STATIC_LIBRARIES := libarect
-LOCAL_HEADER_LIBRARIES := libnativebase_headers
+LOCAL_HEADER_LIBRARIES := display_headers libnativebase_headers
 
 LOCAL_MODULE_PATH := $(TARGET_OUT_SHARED_LIBRARIES)/hw
 LOCAL_MODULE := camera.$(TARGET_BOARD_PLATFORM)
