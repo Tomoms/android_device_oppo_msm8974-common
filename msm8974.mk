b8fd3384067f082c89f9c18d42db01a2be4161e4
Tommaso Fonda <fondatommaso2@gmail.com>
2023-03-12 12:02:10 +0100

    msm8974-common: Enable a few optimizations from Android (Go edition)
    Change-Id: If1218d17857ca269d55b6054de93b0784b42a200

--- msm8974.mk
+++ msm8974.mk
@@ -241,6 +241,11 @@ PRODUCT_COPY_FILES += \
 PRODUCT_PACKAGES += \
     android.hardware.sensors@1.0-impl
 
+# Speed profile services and wifi-service to reduce RAM and storage
+PRODUCT_SYSTEM_SERVER_COMPILER_FILTER := speed-profile
+PRODUCT_USE_PROFILE_FOR_BOOT_IMAGE := true
+PRODUCT_DEX_PREOPT_BOOT_IMAGE_PROFILE_LOCATION := frameworks/base/config/boot-image-profile.txt
+
 # Thermal config
 PRODUCT_COPY_FILES += \
     $(LOCAL_PATH)/configs/thermal-engine.conf:$(TARGET_COPY_OUT_VENDOR)/etc/thermal-engine-8974.conf
